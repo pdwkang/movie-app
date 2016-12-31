5540c6e712158740c64bf76760264be49a5822cc
Paul <dwkang@uga.edu>
2016-12-30 19:03:02 -0500

    tv-shows progress

--- js/scripts.js
+++ js/scripts.js
@@ -12,67 +12,68 @@ $(document).ready(function(){
 			$.getJSON  (trailerUrl, function(targetMovieData){
 				var detailUrl = apiBaseUrl + 'movie/' + targetMovieData.id + '?api_key=' + apiKey
 				$.getJSON(detailUrl, function(detailData){
-					var creditUrl = apiBaseUrl + 'movie/' + detailData.id + '/credits?api_key=' + apiKey
-					$.getJSON(creditUrl, function(creditData){
-						var releaseUrl = apiBaseUrl + 'movie/' + creditData.id + '/release_dates?api_key=' + apiKey	
-						$.getJSON(releaseUrl, function(releaseData){
-							var releaseDataArray = releaseData.results
-							var mpaa = ""
-							for(let i = 0; i<releaseData.results.length; i++){
-								if(releaseData.results[i].iso_3166_1 === "US"){
-									mpaa = releaseData.results[i].release_dates[0].certification
-								}
-							}
-							var cast = creditData.cast[0].name + ", " + creditData.cast[1].name
-							var genre = ""
-							var crew = creditData.crew[0].name
-							var job = creditData.crew[0].job 
-							for(let i = 0; i<detailData.genres.length; i++){
-								if(i>0){
-									if(i<4){
-									genre = genre + ", " + detailData.genres[i].name}
-								}else{genre = detailData.genres[i].name}
-							}
-							var homepage = detailData.homepage;
-							var runtime = detailData.runtime			
-							var movieKey = targetMovieData.results[0].key
-							var youtubeLink = 'http://www.youtube.com/embed/' + movieKey
-							var poster = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
-							if(i===0){nowPlayingHTML += '<div class="item active">'
-							}else{nowPlayingHTML += '<div class="item">'};
-							nowPlayingHTML += '<div class="each-movie">';
-								nowPlayingHTML += '<div class="poster"><img src="' + poster + '"></div>';
-								nowPlayingHTML += '<div class="right-box">';
-									nowPlayingHTML += '<div class="title">' + nowPlayingData.results[i].title
-									if(mpaa.length>0){
-										nowPlayingHTML += ' <span class="mpaa">(' + mpaa + ')</span></div>'
-									}else{nowPlayingHTML += '</div>'}
-									nowPlayingHTML += '<div class="overview">' + nowPlayingData.results[i].overview + '</div>'
-									nowPlayingHTML += '<div class="vote-average">Rating: ' + nowPlayingData.results[i].vote_average + ' / 10 <img src="rating.png"></div>'
-									nowPlayingHTML += '<div class="genre">Genre: ' + genre + '</div>'
-									nowPlayingHTML += '<div class="runtime">Runtime: ' + runtime + ' minutes</div>'
-									nowPlayingHTML += '<div class="cast">Cast: ' + cast + '</div>'
-									nowPlayingHTML += '<div class="cast">Director / Producer: ' + crew + '</div>'
-									nowPlayingHTML += '<div class="release-date">Release Date: ' + nowPlayingData.results[i].release_date + '</div>'									
-									if(homepage.length>1){
-										nowPlayingHTML += '<div class="homepage">Website: <a target="_blank" href="' + homepage + '">' + homepage + '</a></div>'}									
-									// nowPlayingHTML += '<div class="preview">Watch Preview<a href="#" class="btn btn-default" data-toggle="modal" data-target="#videoModal" data-theVideo="' + youtubeLink + '">' + '<img src="youtube.png"></a></div>'		
-									// nowPlayingHTML += '<div class="preview">Watch Preview<a target="_blank" href="'+ youtubeLink + '">' + '<img src="youtube.png"></a></div>'
-									nowPlayingHTML += '<div class="preview">Preview: <a target="_blank" href="'+ youtubeLink + '">' + youtubeLink + '</a></div>'
-								nowPlayingHTML += '</div>'	   	//right-box closing tag
-							nowPlayingHTML += '</div>'			//each-movie closing tag	  youtubeLink
-							nowPlayingHTML += '</div>'			//item closing tag
-							$('.carousel-inner').html(nowPlayingHTML + carouselHTML);
-							if(i<10){
-								if(nowPlayingData.results[i].title.length>27){
-									nowPlayingChartHTML += '<div class="nowPlayingChart">' + nowPlayingData.results[i].title.slice(0,27) +'... &nbsp<span class="nowPlayingRating"><img src="thumbsUp.png"> ' + nowPlayingData.results[i].vote_average*10 + '%</span></div>'
-								}else{nowPlayingChartHTML += '<div class="nowPlayingChart">' + nowPlayingData.results[i].title + '&nbsp<span class="nowPlayingRating"><img src="thumbsUp.png"> ' + nowPlayingData.results[i].vote_average*10 + '%</span></div>'
-								};
-								$('.topNowShowing').html(nowPlayingTitle + nowPlayingChartHTML);
-							}
-						});
-					});
+				////////////////////////////////////	
+				///// reached MAX API REQUESTS /////
+				////////////////////////////////////	
+					// var creditUrl = apiBaseUrl + 'movie/' + detailData.id + '/credits?api_key=' + apiKey
+					// $.getJSON(creditUrl, function(creditData){
+					// 	var releaseUrl = apiBaseUrl + 'movie/' + creditData.id + '/release_dates?api_key=' + apiKey	
+					// 	$.getJSON(releaseUrl, function(releaseData){
+					// 		var releaseDataArray = releaseData.results
+					// 		var mpaa = ""
+					// 		for(let i = 0; i<releaseData.results.length; i++){
+					// 			if(releaseData.results[i].iso_3166_1 === "US"){
+					// 				mpaa = releaseData.results[i].release_dates[0].certification
+					// 			}
+					// 		}
+					// 		var cast = creditData.cast[0].name + ", " + creditData.cast[1].name
+					// 		var crew = creditData.crew[0].name
+					// 		var job = creditData.crew[0].job 
+					var genre = ""
+					for(let i = 0; i<detailData.genres.length; i++){
+						if(i>0){
+							if(i<4){
+							genre = genre + ", " + detailData.genres[i].name}
+						}else{genre = detailData.genres[i].name}
+					}
+					var homepage = detailData.homepage;
+					var runtime = detailData.runtime			
+					var movieKey = targetMovieData.results[0].key
+					var youtubeLink = 'http://www.youtube.com/embed/' + movieKey
+					var poster = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
+					if(i===0){nowPlayingHTML += '<div class="item active">'
+					}else{nowPlayingHTML += '<div class="item">'};
+					nowPlayingHTML += '<div class="each-movie">';
+						nowPlayingHTML += '<div class="poster"><img src="' + poster + '"></div>';
+						nowPlayingHTML += '<div class="right-box">';
+							nowPlayingHTML += '<div class="title">' + nowPlayingData.results[i].title + '</div>'
+							// if(mpaa.length>0){
+							// 	nowPlayingHTML += ' <span class="mpaa">(' + mpaa + ')</span></div>'
+							// }else{nowPlayingHTML += '</div>'}
+							nowPlayingHTML += '<div class="overview">' + nowPlayingData.results[i].overview + '</div>'
+							nowPlayingHTML += '<div class="vote-average">Rating: ' + nowPlayingData.results[i].vote_average + ' / 10 <img src="rating.png"></div>'
+							nowPlayingHTML += '<div class="genre">Genre: ' + genre + '</div>'
+							nowPlayingHTML += '<div class="runtime">Runtime: ' + runtime + ' minutes</div>'
+							// nowPlayingHTML += '<div class="cast">Cast: ' + cast + '</div>'
+							// nowPlayingHTML += '<div class="cast">Director / Producer: ' + crew + '</div>'
+							nowPlayingHTML += '<div class="release-date">Release Date: ' + nowPlayingData.results[i].release_date + '</div>'									
+							if(homepage.length>1){
+								nowPlayingHTML += '<div class="homepage">Website: <a target="_blank" href="' + homepage + '">' + homepage + '</a></div>'}									
+							nowPlayingHTML += '<div class="preview">Preview: <a target="_blank" href="'+ youtubeLink + '">' + youtubeLink + '</a></div>'
+						nowPlayingHTML += '</div>'	   	//right-box closing tag
+						nowPlayingHTML += '</div>'			//each-movie closing tag	  youtubeLink
+						nowPlayingHTML += '</div>'			//item closing tag
+						$('.carousel-inner').html(nowPlayingHTML + carouselHTML);
+					if(i<10){
+						if(nowPlayingData.results[i].title.length>27){
+							nowPlayingChartHTML += '<div class="nowPlayingChart">' + nowPlayingData.results[i].title.slice(0,27) +'... &nbsp<span class="nowPlayingRating"><img src="thumbsUp.png"> ' + nowPlayingData.results[i].vote_average*10 + '%</span></div>'
+						}else{nowPlayingChartHTML += '<div class="nowPlayingChart">' + nowPlayingData.results[i].title + '&nbsp<span class="nowPlayingRating"><img src="thumbsUp.png"> ' + nowPlayingData.results[i].vote_average*10 + '%</span></div>'
+						};
+						$('.topNowShowing').html(nowPlayingTitle + nowPlayingChartHTML);
+					}
 				});
+				// 	});
+				// });
 			})
 		} //for loop ends
 	});
@@ -85,11 +86,9 @@ $(document).ready(function(){
 		searchInput = $('#movieSearch').val();		
 		searchMovie();
 	});
-
 	function searchMovie(){
 		var fullSearch = searchMovieUrl + searchInput;
 		$.getJSON(fullSearch, function(movieSearched){
-			console.log(movieSearched)
 			for(let i = 0; i < movieSearched.results.length; i++){
 				var posterSearched = imageBaseUrl + 'w300' + movieSearched.results[i].poster_path;
 				var searchMovieTitle = movieSearched.results[i].title;
@@ -115,12 +114,29 @@ $(document).ready(function(){
 		})
 	}	//searchMovie function ends
     $('.search-results').on("click", '.eachSearchMovie', function(){
-    	$('.searchMovieDescription').hide()
-		$(this).children('.searchMovieDescription').fadeIn()
-		// console.log(this)
+    	$('.searchMovieDescription').hide();
+    	var $thisPoster = $(this).children('.searchMovieDescription')
+		$thisPoster.fadeIn();
+		setTimeout(function(){$thisPoster.fadeOut(1300);}, 7000)
 	})
-});
 
+	function getTvShows(){
+		const tvShowUrl = apiBaseUrl + 'tv/top_rated?api_key=' + apiKey 
+		var tvShowHTML = ''
+	    $.getJSON(tvShowUrl, function(tvData){
+	    	console.log(tvData)
+			for(let i = 0; i < tvData.results.length; i++){
+				tvShowHTML += '<div class="eachTvShow">'
+					tvShowHTML += '<img src="' + imageBaseUrl + 'w300' + tvData.results[i].poster_path + '"><br>'
+					tvShowHTML += '<span class="tvTitle">' + tvData.results[i].name + '</span>'
+					// tvShowHTML += '<span class="tvDescription">' + tvData.results[i].overview + '</span>'
+				tvShowHTML += '</div>'	
+				$('.tv-wrapper').html(tvShowHTML)
+			}
+		});
+	}
+	getTvShows();
+});
 
 var carouselHTML = `<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
     			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -128,11 +144,7 @@ var carouselHTML = `<a class="left carousel-control" href="#carousel-example-gen
   				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
     			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
     			<span class="sr-only">Next</span></a>`
-
 var nowPlayingTitle = '<div class="nowPlayingTitle">Top Movies In Theaters Now!</div>'
-
-
-
 
 ////////////////////
 ///WEATHER WIDGET///
